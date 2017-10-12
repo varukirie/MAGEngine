f808aaee7324532a04f9331626201879075c8151
Eland_aza <1017814810@qq.com>
2017-10-12 13:01:08 +0800

    smallerplayerimgfix and starfollowup

--- src/magengine/chapter/TestChapter1.java
+++ src/magengine/chapter/TestChapter1.java
@@ -10,6 +10,7 @@ import magengine.bullet.StarBullet;
 import magengine.chapter.util.AChapter;
 import magengine.chapter.util.QuickDanmuku;
 import magengine.element.BaseElement;
+import magengine.element.impl.Player;
 import magengine.enemy.DefaultEnemy;
 import magengine.enemy.Enemy1;
 import magengine.launcher.Launcher;
@@ -44,34 +45,40 @@ public class TestChapter1 extends AChapter {
 		double x = 275,y = 195;
 		long delay = 1500 ,interval = 200;
 		long runningtime = 1500;
+		Player p = Player.getPlayer();
 		QuickDanmuku quick = QuickDanmuku.getQuickDanmuku();
-		DefaultEnemy enemy = new DefaultEnemy(0+x,starlength*triFunc(18,2)+y);//敌人初始化位置有问题！！！
-		Launcher launcher = new Launcher(300, 200, Math.PI / 2, 100, delay+4*(runningtime+interval));
-		launcher.setBulletSpeed(0.00001);
+		DefaultEnemy enemy = new DefaultEnemy(0+x,starlength*triFunc(18,2)+y);
+		Launcher launcher = new Launcher(0+x, starlength*triFunc(18,2)+y, Math.PI / 2, 100, delay+5*(runningtime+interval));
+		launcher.setBulletSpeed(1);
 		launcher.setInterval(10);
 		launcher.setBulletType(StarBullet.class);
-		mEU.add("enemy", enemy);
-		mEU.add("testlauncher", launcher);
-		enemy.getxProperty().bindBidirectional(launcher.getxProperty());
-		enemy.getyProperty().bindBidirectional(launcher.getyProperty());
+		sES.schedule(()->{
+			mEU.add("enemy", enemy);
+			mEU.add("testlauncher", launcher);
+		}, 1000, TimeUnit.MILLISECONDS);
+		launcher.getxProperty().bind(enemy.getxProperty());
+		launcher.getyProperty().bind(enemy.getyProperty());
 		launcher.setBulletEvent((executor,bullet)->{
 			executor.schedule(()->{
-				bullet.setVelocityY(50);
-			}, delay+4*(runningtime+interval), TimeUnit.MILLISECONDS);
+				quick.runBullet(bullet);
+				quick.VTo(bullet, p.getX() , p.getY());
+			}, delay+5*(runningtime+interval), TimeUnit.MILLISECONDS);
 		});
-		quick.moveTo(enemy, runningtime, -starlength*triFunc(36,2)+x,-starlength*triFunc(36,2)*triFunc(18,3)+starlength*triFunc(18,2)+y );
+		sES.schedule(()->{
+			quick.moveTo(enemy, runningtime, -starlength*triFunc(36,2)+x,-starlength*triFunc(36,2)*triFunc(18,3)+starlength*triFunc(18,2)+y );
+		},delay,TimeUnit.MILLISECONDS);
 		sES.schedule(() -> {
 			quick.moveTo(enemy, runningtime, starlength+starlength*triFunc(18,1)+x, 0+y);
-		}, delay, TimeUnit.MILLISECONDS);
-		sES.schedule(() -> {
-			quick.moveTo(enemy, runningtime, -starlength-starlength*triFunc(18,1)+x, 0+y);
 		}, delay+(runningtime+interval), TimeUnit.MILLISECONDS);
 		sES.schedule(() -> {
-			quick.moveTo(enemy, runningtime, starlength*triFunc(36,2)+x, -starlength*triFunc(36,2)*triFunc(18,3)+starlength*triFunc(18,2)+y);
+			quick.moveTo(enemy, runningtime, -starlength-starlength*triFunc(18,1)+x, 0+y);
 		}, delay+2*(runningtime+interval), TimeUnit.MILLISECONDS);
 		sES.schedule(() -> {
-			quick.moveTo(enemy, runningtime, 0+x, starlength*triFunc(18,2)+y);
+			quick.moveTo(enemy, runningtime, starlength*triFunc(36,2)+x, -starlength*triFunc(36,2)*triFunc(18,3)+starlength*triFunc(18,2)+y);
 		}, delay+3*(runningtime+interval), TimeUnit.MILLISECONDS);
+		sES.schedule(() -> {
+			quick.moveTo(enemy, runningtime, 0+x, starlength*triFunc(18,2)+y);
+		}, delay+4*(runningtime+interval), TimeUnit.MILLISECONDS);
 	
 //		long delay = 1000;
 //		double bulletspeed=0.5;
