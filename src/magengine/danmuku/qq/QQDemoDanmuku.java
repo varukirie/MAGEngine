ef2fa3ea528ef6ec314a5c2c5124e1553150bba8
TRsky <625310581@qq.com>
2017-11-18 18:39:19 +0800

    add a new danmuku

--- src/magengine/danmuku/qq/QQDemoDanmuku.java
+++ src/magengine/danmuku/qq/QQDemoDanmuku.java
@@ -1,8 +1,10 @@
 package magengine.danmuku.qq;
 
+import magengine.bullet.impl.StarBullet;
+import magengine.chapter.util.QuickDanmuku;
 import magengine.danmuku.ADanmuku;
-import magengine.launcher.BurstLauncher;
 import magengine.launcher.Launcher;
+import magengine.launcher.OvalLauncherGroup;
 import magengine.util.ElementUtils;
 
 import java.util.concurrent.TimeUnit;
@@ -12,19 +14,49 @@ public class QQDemoDanmuku extends ADanmuku {
 	@Override
 	public void executeDanmuku() {
 		ElementUtils mEU= super.getmEU();
-		long delay = 2000;
-		Launcher launcher = new BurstLauncher(300, 100, Math.PI/2, 3000, 10000);
-		launcher.setBulletEvent((executor, bullet) -> {
+		int delay = 3000;
+		Launcher startLauncher = new Launcher(300, 200, Math.PI / 2, 20, delay);
+		startLauncher.setBulletType(StarBullet.class);
+		startLauncher.setBulletEvent((executor, bullet)->{
 			executor.schedule(()->{
-				bullet.setVelocityX(140);
-				bullet.setVelocityY(0);
-			}, delay, TimeUnit.MILLISECONDS);
-			executor.schedule(()->{
-				bullet.setVelocityX(0);
-				bullet.setVelocityY(100);
-			}, delay+2000, TimeUnit.MILLISECONDS);
+				bullet.setVelocityX(Math.random()*300*Math.pow(-1, (int)(Math.random()*10)));
+				bullet.setVelocityY(Math.random()*300*Math.pow(-1, (int)(Math.random()*10)));
+			}, 0, TimeUnit.MILLISECONDS);
 		});
-		mEU.add("testLauncher", launcher);
+		mEU.add("startLauncher", startLauncher);
+		//Բ�ε�Ļ
+		QuickDanmuku quick = QuickDanmuku.getQuickDanmuku();
+		OvalLauncherGroup oLG = new OvalLauncherGroup(300, 200, 100);
+		oLG.setLauncherConfig((ovalLauncher)->{
+			ovalLauncher.setBulletEvent((sESx, bullet)->{
+				sESx.schedule(()->{
+					quick.stopBullet(bullet);
+				}, 2000, TimeUnit.MILLISECONDS);
+				sESx.schedule(()->{
+					quick.runBullet(bullet);
+					quick.VTransform(bullet, new double[][]{
+						{Math.pow(-1, (int)(Math.random()*10)), 0}, {0, Math.pow(-1, (int)(Math.random()*10)) }
+					});
+				}, 4000, TimeUnit.MILLISECONDS);
+			});
+		});
+		oLG.delayExecute(500);
+		
+		OvalLauncherGroup smallOLG = new OvalLauncherGroup(300, 200, 200);
+		smallOLG.setLauncherConfig((ovalLauncher)->{
+			ovalLauncher.setBulletEvent((sESx, bullet)->{
+				sESx.schedule(()->{
+					quick.stopBullet(bullet);
+				}, 1000, TimeUnit.MILLISECONDS);
+				sESx.schedule(()->{
+					quick.runBullet(bullet);
+					quick.VTransform(bullet, new double[][]{
+						{0, Math.pow(-1, (int)(Math.random()*10))}, {Math.pow(-1, (int)(Math.random()*10)),0 }
+					});
+				}, 2000, TimeUnit.MILLISECONDS);
+			});
+		});
+		smallOLG.delayExecute(500);
 		
 	}
 
