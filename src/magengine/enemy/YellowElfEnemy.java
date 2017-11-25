7df75dec605f48bbc4379bd34cc81cc7ba342a1f
TRsky <625310581@qq.com>
2017-11-25 21:56:43 +0800

    add elfs

--- src/magengine/enemy/YellowElfEnemy.java
+++ src/magengine/enemy/YellowElfEnemy.java
@@ -4,21 +4,21 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import magengine.paint.SpritePainter;
 
-public class ElfEnemy extends ALoopDanmukuEnemy {
+public class YellowElfEnemy extends ALoopDanmukuEnemy {
 	private static SpritePainter spritePainter = null;
 	
-	public ElfEnemy(double x, double y) {
+	public YellowElfEnemy(double x, double y) {
 		super(x, y);
 	}
 	private static double[][] origin = null;
-	private static int enemyWidth = 29;
-	private static int enemyHeight = 39;
+	private static int enemyWidth = 33;
+	private static int enemyHeight = 34;
 	private static int halfWidth = enemyWidth/2;
 	private static int halfHeight = enemyHeight/2;
 	private static Image img=null;
 	static {
 		try {
-			img = new Image(ElfEnemy.class.getResourceAsStream("/img/enemies/elfEnemy.png"));
+			img = new Image(YellowElfEnemy.class.getResourceAsStream("/img/enemies/yellowElfEnemy01.png"));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -34,9 +34,9 @@ public class ElfEnemy extends ALoopDanmukuEnemy {
 	@Override
 	public void paint(GraphicsContext gc) {
 		super.paint(gc);
-		int count=4;
+		int count=5;
 		//每隔interval帧换一张图
-		int interval = 5;
+		int interval = 3;
 		spritePainter.paintSprite((0+(currentSpriteIndex))/interval, getX()-halfWidth, getY()-halfHeight, gc);
 		currentSpriteIndex=(currentSpriteIndex+spriteDelta)%(count*interval);
 		if(currentSpriteIndex==count*interval-1||currentSpriteIndex==0){
